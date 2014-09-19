5c4f53d56403250acce3db2167430973d566bb05
Joris Bontje <jorisbontje@gmail.com>
2014-09-19 13:48:36 +0200

    generated assets

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -21,10 +21,10 @@ module.exports = {
       { test: /\.less$/, loader: "style!css!less" },
       { test: /\.jsx$/, loaders: ["react-hot", "jsx"] },
       { test: /\.json$/, loader: "json" },
-      { test: /\.woff$/,   loader: "url-loader?limit=10000&minetype=application/font-woff" },
-      { test: /\.ttf$/,    loader: "file-loader" },
-      { test: /\.eot$/,    loader: "file-loader" },
-      { test: /\.svg$/,    loader: "file-loader" }
+      { test: /\.woff$/,   loader: "url-loader?limit=10000&minetype=application/font-woff&name=assets/[name]-[hash].[ext]" },
+      { test: /\.ttf$/,    loader: "file-loader?name=assets/[name]-[hash].[ext]" },
+      { test: /\.eot$/,    loader: "file-loader?name=assets/[name]-[hash].[ext]" },
+      { test: /\.svg$/,    loader: "file-loader?name=assets/[name]-[hash].[ext]" }
     ]
   }
 };
