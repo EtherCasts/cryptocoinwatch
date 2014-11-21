ae2144964d2d7ba6f465541452e5de5edf1d6948
Joris Bontje <jorisbontje@gmail.com>
2014-11-21 12:31:35 +0100

    update example_btc_trade to serpent 2.0 / poc7

--- test/test_example_btc_trade.py
+++ test/test_example_btc_trade.py
@@ -1,11 +1,6 @@
 from pyethereum import tester
 
-import logging
-
-from cryptocoinwatch.utils import address_to_hex, hex_pad, xhex
-
-# from pyethereum import processblock
-# processblock.enable_debug()
+from cryptocoinwatch.utils import address_to_hex, xhex
 
 
 class TestExampleBtcTrade(object):
@@ -15,44 +10,46 @@ class TestExampleBtcTrade(object):
     BTC_AMOUNT = 100000  # in satoshi
     ETH_AMOUNT = 2 * 10 ** 18  # in wei
 
-    @classmethod
-    def setup_class(cls):
-        logging.disable(logging.INFO)  # disable DEBUG logging of pyethereum.processblock
-        cls.watch_code = open('contracts/cryptocoinwatch.se').read()
-        cls.trade_code = open('contracts/example_btc_trade.se').read()
+    WATCH_CONTRACT = 'contracts/cryptocoinwatch.se'
+    TRADE_CONTRACT = 'contracts/example_btc_trade.se'
+
+    WATCH_FUN_WATCH = 3
+    WATCH_FUN_SETRECEIVEDBYADDRESS = 6
+
+    TRADE_FUN_NEW = 0
+    TRADE_FUN_FINISH = 1
 
     def setup_method(self, method):
         self.s = tester.state()
-        self.watch_contract = self.s.contract(self.watch_code)
-        self.trade_contract = self.s.contract(self.trade_code)
+        self.watch_contract = self.s.contract(self.WATCH_CONTRACT)
+        self.trade_contract = self.s.contract(self.TRADE_CONTRACT)
 
     def _storage(self, idx):
-        idx = hex_pad(idx)
         return self.s.block.account_to_dict(self.trade_contract)['storage'].get(idx)
 
     def test_init(self):
-        assert self._storage(0x10) == '0x' + tester.a0  # buyer
-        assert self._storage(0x30) == '0x01'  # state = new
+        assert self._storage('0x') == '0x' + tester.a0  # buyer
+        assert self._storage('0x06') == '0x01'  # state = new
 
     def test_watch_feed(self):
-        o1 = self.s.send(tester.k0, self.watch_contract, 0, ['watch', self.BTC_ADDRESS])
+        o1 = self.s.send(tester.k0, self.watch_contract, 0, funid=self.WATCH_FUN_WATCH, abi=[self.BTC_ADDRESS])
         assert o1 == [1]
 
     def test_new(self):
         buyer_balance = self.s.block.get_balance(tester.a0)
 
         expiry = self.s.block.timestamp + 86400
-        o1 = self.s.send(tester.k0, self.trade_contract, self.ETH_AMOUNT,
-                         ['new', self.SELLER, int(self.watch_contract, 16), self.BTC_ADDRESS, self.BTC_AMOUNT, expiry])
+        o1 = self.s.send(tester.k0, self.trade_contract, self.ETH_AMOUNT, funid=self.TRADE_FUN_NEW,
+                         abi=[self.SELLER, int(self.watch_contract, 16), self.BTC_ADDRESS, self.BTC_AMOUNT, expiry])
         assert o1 == [2]  # pending
 
-        assert self._storage(0x10) == '0x' + tester.a0  # buyer
-        assert self._storage(0x11) == '0x' + tester.a1  # seller
-        assert self._storage(0x12) == '0x' + self.watch_contract
-        assert self._storage(0x20) == xhex(self.BTC_ADDRESS)
-        assert self._storage(0x21) == xhex(self.BTC_AMOUNT)
-        assert self._storage(0x22) == xhex(expiry)
-        assert self._storage(0x30) == '0x02'  # state = pending
+        assert self._storage('0x') == '0x' + tester.a0  # buyer
+        assert self._storage('0x01') == '0x' + tester.a1  # seller
+        assert self._storage('0x02') == '0x' + self.watch_contract
+        assert self._storage('0x03') == xhex(self.BTC_ADDRESS)
+        assert self._storage('0x04') == xhex(self.BTC_AMOUNT)
+        assert self._storage('0x05') == xhex(expiry)
+        assert self._storage('0x06') == '0x02'  # state = pending
 
         # amount escrowed
         assert self.s.block.get_balance(self.trade_contract) == self.ETH_AMOUNT
@@ -63,11 +60,11 @@ class TestExampleBtcTrade(object):
         seller_balance = self.s.block.get_balance(tester.a1)
 
         expiry = self.s.block.timestamp + 86400
-        o1 = self.s.send(tester.k0, self.trade_contract, self.ETH_AMOUNT,
-                         ['new', self.SELLER, int(self.watch_contract, 16), self.BTC_ADDRESS, self.BTC_AMOUNT, expiry])
+        o1 = self.s.send(tester.k0, self.trade_contract, self.ETH_AMOUNT, funid=self.TRADE_FUN_NEW,
+                         abi=[self.SELLER, int(self.watch_contract, 16), self.BTC_ADDRESS, self.BTC_AMOUNT, expiry])
         assert o1 == [2]  # pending
 
-        o2 = self.s.send(tester.k0, self.trade_contract, 0, ['finish'])
+        o2 = self.s.send(tester.k0, self.trade_contract, 0, funid=self.TRADE_FUN_FINISH, abi=[])
         assert o2 == [2]  # still pending
 
         # amount escrowed
@@ -80,15 +77,16 @@ class TestExampleBtcTrade(object):
         seller_balance = self.s.block.get_balance(tester.a1)
 
         expiry = self.s.block.timestamp + 86400
-        o1 = self.s.send(tester.k0, self.trade_contract, self.ETH_AMOUNT,
-                         ['new', self.SELLER, int(self.watch_contract, 16), self.BTC_ADDRESS, self.BTC_AMOUNT, expiry])
+        o1 = self.s.send(tester.k0, self.trade_contract, self.ETH_AMOUNT, funid=self.TRADE_FUN_NEW,
+                         abi=[self.SELLER, int(self.watch_contract, 16), self.BTC_ADDRESS, self.BTC_AMOUNT, expiry])
         assert o1 == [2]  # pending
 
         # update datafeed
-        o2 = self.s.send(tester.k0, self.watch_contract, 0, ['setreceivedbyaddress', self.BTC_ADDRESS, self.BTC_AMOUNT])
+        o2 = self.s.send(tester.k0, self.watch_contract, 0, funid=self.WATCH_FUN_SETRECEIVEDBYADDRESS,
+                         abi=[self.BTC_ADDRESS, self.BTC_AMOUNT])
         assert o2 == [1]
 
-        o3 = self.s.send(tester.k0, self.trade_contract, 0, ['finish'])
+        o3 = self.s.send(tester.k0, self.trade_contract, 0, funid=self.TRADE_FUN_FINISH, abi=[])
         assert o3 == [3]  # finished
 
         # seller is paid
@@ -101,8 +99,8 @@ class TestExampleBtcTrade(object):
         seller_balance = self.s.block.get_balance(tester.a1)
 
         expiry = self.s.block.timestamp + 86400
-        o1 = self.s.send(tester.k0, self.trade_contract, self.ETH_AMOUNT,
-                         ['new', self.SELLER, int(self.watch_contract, 16), self.BTC_ADDRESS, self.BTC_AMOUNT, expiry])
+        o1 = self.s.send(tester.k0, self.trade_contract, self.ETH_AMOUNT, funid=self.TRADE_FUN_NEW,
+                         abi=[self.SELLER, int(self.watch_contract, 16), self.BTC_ADDRESS, self.BTC_AMOUNT, expiry])
         assert o1 == [2]  # pending
 
         assert self.s.block.get_balance(self.trade_contract) == self.ETH_AMOUNT
@@ -111,7 +109,7 @@ class TestExampleBtcTrade(object):
 
         # move the time forward past expiry
         self.s.block.timestamp = expiry + 1
-        o2 = self.s.send(tester.k0, self.trade_contract, 0, ['finish'])
+        o2 = self.s.send(tester.k0, self.trade_contract, 0, funid=self.TRADE_FUN_FINISH, abi=[])
         assert o2 == [4]  # expired
 
         # buyer is refunded
